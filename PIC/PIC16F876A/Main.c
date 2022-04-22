f81340e025ae113170c713bd4984fcdfc2268c7b
BeFlE <83487780+BeFlE@users.noreply.github.com>
2022-04-22 12:52:12 +0200

    Update Main.c

--- PIC/PIC16F876A/Main.c
+++ PIC/PIC16F876A/Main.c
@@ -1,11 +1,3 @@
-/*
- * File:   Main.c
- * Author: Heino
- *
- * Created on 14. Februar 2021, 13:03
- */
-
-
 #include <xc.h>
 #include "LCD_header.h"
 #include "Main-Header.h"
@@ -15,8 +7,6 @@
 #include <pic16f876a.h>
 
 volatile unsigned long millisec;
-unsigned char AckDevices[20];
-unsigned char PWM[10], NrOfChannels[10];
 
 void __interrupt(high_priority) isr(void)
 {  
@@ -30,58 +20,16 @@ void __interrupt(high_priority) isr(void)
 void main(void) 
 {
     
-//    bool ZeileEinsVoll = false;
     Init();
     
-//    startI2C();
-//    sendI2C((127<<1) & 0xFE);
-//    sendI2C('A');
-//    I2CAdr = 0xaa;
-//    sendI2C(I2CAdr<<1);
-//    stopI2C();
-    
-//    startI2C();
-//    sendI2C((0x40) & 0xFE);
-//    sendI2C('A');
-//    sendI2C(0xaa);
-////    sendI2C(0x10);
-//    stopI2C();
-    
-    
-//    startI2C();
-//    sendI2C((I2CAdr) | 0x01);
-//    LPValue = reciveI2C_ack();
-//    Value = reciveI2C_nack();
-//    stopI2C();
-//    sprintf((char*)ausgabe,(const char*)"A:%u L:%u V:%u",I2CAdr,LPValue,Value);
-//    LCD_NewData((const char*)ausgabe,LCD_Zeile1,LCD_linksbuendig);
-//    lcd_writeArray();
-//    
     while (1)
     {
         CLRWDT();
-        if(PORTBbits.RB5)
+        if(millisec % 1000 <= 3)
         {
-            if(millisec % 300 <= 5)
-            {
-                millisec +=5;
-                LEDDriver();
-            } 
-            if(millisec % 1000 <= 3)
-            {
-                millisec += 3;
-                CheckLedDriver();
-                DisplayLedDriver();
-            }
-        }
-        else
-        {
-            if(millisec % 1000 <= 3)
-            {
-                millisec += 3;
-                SoMoSe();
-            }  
-        }
+            millisec += 3;
+            SoMoSe();
+        }  
     }
 }
 void Init(void)
@@ -100,16 +48,8 @@ void Init(void)
  
     TRISBbits.TRISB5 = 1;
    
-    if(PORTBbits.RB5)
-    {//I2CLEDDriver
-        ADCON1 = 0x07;  
-        TRISAbits.TRISA0 = 1;//
-    }
-    else
-    {//SoMoSe
-        ADCON1 = 0x00;
-        ADCON0bits.ADON = 1;
-    }
+    ADCON1 = 0x00;
+    ADCON0bits.ADON = 1;
 }
 void SoMoSe(void)
 {
@@ -118,19 +58,6 @@ void SoMoSe(void)
     unsigned char I2CAdr = 0xAA;
     signed char Temp;
     
-    //            startI2C();
-                    //            sendI2C((I2CAdr) & 0xFE);
-                    //            sendI2C('v');
-                    //            stopI2C();
-                    //            startI2C();
-                    //            sendI2C((I2CAdr) | 0x01);
-                    //            LPValue = reciveI2C_ack();
-                    //            Value = reciveI2C_nack();
-                    //            stopI2C();
-                    //            sprintf((char*)ausgabe,(const char*)"A:%u L:%u V:%u",I2CAdr,LPValue,Value);
-                    //            LCD_NewData((const char*)ausgabe,LCD_Zeile1,LCD_linksbuendig);
-                    //            lcd_writeArray();
-//            ZeileEinsVoll = false;
             for(I2CAdr = 0; I2CAdr < 128; I2CAdr++)
             {//scan all I2C-adr, 7Bit!
                 startI2C();
@@ -184,84 +111,6 @@ void SoMoSe(void)
                 LCD_NewData((const char*)ausgabe,LCD_Zeile2,LCD_linksbuendig);
                 lcd_writeArray();
             }
-            
-//            startI2C();
-//            sendI2C(I2CAdr | 0x01);
-//            LPValue = reciveI2C_ack();
-//            Value = reciveI2C_nack();
-//            stopI2C();
-////            ADCON0bits.GO_nDONE = 1;
-////            while(ADCON0bits.GO_DONE);
-//            sprintf((char*)ausgabe,(const char*)"%u - %u",LPValue,Value);
-//            LCD_NewData((const char*)ausgabe,LCD_Zeile1,LCD_linksbuendig);
-//            lcd_writeArray();
-}
-void LEDDriver(void)
-{
-    static unsigned char i;
-    unsigned char Nr = 0;
-
-    if(!PORTAbits.RA0)
-        return;
-    while(AckDevices[Nr])
-    {
-        startI2C();
-        sendI2C((AckDevices[Nr] << 1)&0xFE);
-        if(NrOfChannels == 4)
-            sendI2C(i++);
-        if(NrOfChannels >= 3)
-        {
-            sendI2C(i++);
-            sendI2C(i++);
-        }
-        sendI2C(i++);
-        stopI2C();
-        Nr ++;
-        i++;
-    }
-}
-void CheckLedDriver(void)
-{
-    unsigned char i = 0;
-    
-    if(!PORTAbits.RA0)
-        return;
-    for(unsigned char I2CAdr = 0; I2CAdr < 128; I2CAdr++)
-    {//scan all I2C-adr, 7Bit!
-        startI2C();
-        if(sendI2C((I2CAdr << 1) | 0x01))//ACK from Slave recieved?
-        {//No ACK recieved, stop I2C and continue with next I2C-adr
-            stopI2C();
-            __delay_ms(1);
-            continue;
-        }
-        AckDevices[i] = I2CAdr;
-        i++;
-        stopI2C();
-    }
-}
-void DisplayLedDriver(void)
-{
-    unsigned char ausgabe[20];
-    unsigned char i = 0;
-    
-    if(!PORTAbits.RA0)
-        return;
-    sprintf((char*)ausgabe,(const char*)"%u,%u,%u",AckDevices[0],AckDevices[1],AckDevices[2]);
-    LCD_NewData((const char*)ausgabe,LCD_Zeile1,LCD_linksbuendig);
-    
-    while(AckDevices[i])
-    {
-        startI2C();
-        sendI2C((AckDevices[i] << 1) | 0x01);
-        NrOfChannels[i] = reciveI2C_ack();
-        PWM[i] = reciveI2C_nack();
-        stopI2C();
-        i++;
-    }
-    sprintf((char*)ausgabe,(const char*)"%u,%u,%u",NrOfChannels[0],NrOfChannels[1],NrOfChannels[2]);
-    LCD_NewData((const char*)ausgabe,LCD_Zeile2,LCD_linksbuendig);
-    lcd_writeArray();
 }
 
 void InitI2C(void)
@@ -278,23 +127,23 @@ void InitI2C(void)
 bool startI2C(void)
 {
     if(idleI2C())                     // Ist der Bus frei?
-        return 0;                     // Fehler im Bus/länger nicht frei
-    SSPCON2bits.SEN = 1;             // Auslösen einer START-Sequenze
-    while( SSPCON2bits.SEN );        // Beendet, wenn SEN gelöscht wurde*
+        return 0;                     // Fehler im Bus/lÃ¤nger nicht frei
+    SSPCON2bits.SEN = 1;             // AuslÃ¶sen einer START-Sequenze
+    while( SSPCON2bits.SEN );        // Beendet, wenn SEN gelÃ¶scht wurde*
     return 1;
 }
 void stopI2C(void)
 {
     idleI2C();                        // Ist der Bus frei?
-    SSPCON2bits.PEN = 1;             // Auslösen einer RESTART-Sequenze
-    while( SSPCON2bits.PEN );        // Beendet, wenn RSEN gelöscht wurde*
+    SSPCON2bits.PEN = 1;             // AuslÃ¶sen einer RESTART-Sequenze
+    while( SSPCON2bits.PEN );        // Beendet, wenn RSEN gelÃ¶scht wurde*
     // *Oder das Bit SSPxIF gesetzt wurde (siehe Datenblatt)
 }
 bool idleI2C(void)
 {
     unsigned long Time = millisec;
     
-    while(SSPSTATbits.R_W)        // Läuft eine Übertragung?
+    while(SSPSTATbits.R_W)        // LÃ¤uft eine Ãœbertragung?
     {
         CLRWDT();
         if(Time + 200 < millisec)
@@ -311,10 +160,10 @@ bool idleI2C(void)
 }
 unsigned char sendI2C(unsigned char byte)
 {
-    idleI2C();                        // Ist der Bus verfügbar?
-    PIR1bits.SSPIF = 0;              // Flag löschen (wird in waitI2C() abgefragt)
-    SSPBUF = byte;                   // Durch füllen des Puffers Sendevorgang auslösen
-    if(waitI2C())                     // Warten bis Übertragung abgeschlossen ist
+    idleI2C();                        // Ist der Bus verfÃ¼gbar?
+    PIR1bits.SSPIF = 0;              // Flag lÃ¶schen (wird in waitI2C() abgefragt)
+    SSPBUF = byte;                   // Durch fÃ¼llen des Puffers Sendevorgang auslÃ¶sen
+    if(waitI2C())                     // Warten bis Ãœbertragung abgeschlossen ist
         return 1;
 
     return (unsigned)SSPCON2 & 0x40; // Return 0: ACK empfangen, 0x40: kein ACK empfangen
@@ -324,10 +173,10 @@ unsigned char reciveI2C_nack(void)
     unsigned char incomming = 0;            // Einlesepuffer
     unsigned long Time = millisec;
 
-    idleI2C();                        // Ist der Bus verfügbar?
-    PIR1bits.SSPIF = 0;              // Flag löschen (wird in waitI2C() abgefragt)
-    SSPCON2bits.RCEN = 1;            // Als Empfänger konfigurieren (wird autom. wieder gelöscht)
-    waitI2C();                        // Warten bis Übertragung (lesend) abgeschlossen ist
+    idleI2C();                        // Ist der Bus verfÃ¼gbar?
+    PIR1bits.SSPIF = 0;              // Flag lÃ¶schen (wird in waitI2C() abgefragt)
+    SSPCON2bits.RCEN = 1;            // Als EmpfÃ¤nger konfigurieren (wird autom. wieder gelÃ¶scht)
+    waitI2C();                        // Warten bis Ãœbertragung (lesend) abgeschlossen ist
     SSPCON2bits.ACKDT = 1;           // Mit NACK quitieren (Nicht weiter einlesen)
     SSPCON2bits.ACKEN = 1;           // NACK aussenden
     while( SSPCON2bits.ACKEN )      // NACK abgeschlossen?
@@ -337,17 +186,17 @@ unsigned char reciveI2C_nack(void)
             return 0xFF;
     }
     incomming = SSPBUF;              // Empfangenes Byte in den Puffer
-    return incomming;                 // Und zurück geben
+    return incomming;                 // Und zurÃ¼ck geben
 }
 unsigned char reciveI2C_ack(void)
 {
     unsigned char incomming = 0;            // Einlesepuffer
     unsigned long Time = millisec;
 
-    idleI2C();                        // Ist der Bus verfügbar?
-    PIR1bits.SSPIF = 0;              // Flag löschen (wird in waitI2C() abgefragt)
-    SSPCON2bits.RCEN = 1;            // Als Empfänger konfigurieren (wird autom. wieder gelöscht)
-    waitI2C();                        // Warten bis Übertragung (lesend) abgeschlossen ist
+    idleI2C();                        // Ist der Bus verfÃ¼gbar?
+    PIR1bits.SSPIF = 0;              // Flag lÃ¶schen (wird in waitI2C() abgefragt)
+    SSPCON2bits.RCEN = 1;            // Als EmpfÃ¤nger konfigurieren (wird autom. wieder gelÃ¶scht)
+    waitI2C();                        // Warten bis Ãœbertragung (lesend) abgeschlossen ist
     SSPCON2bits.ACKDT = 0;           // Mit AACK quitieren (weiter einlesen)
     SSPCON2bits.ACKEN = 1;           // ACK aussenden
     while( SSPCON2bits.ACKEN )      // ACK abgeschlossen?
@@ -358,7 +207,7 @@ unsigned char reciveI2C_ack(void)
     }
     incomming = SSPBUF;              // Empfangenes Byte in den Puffer
 
-    return incomming;                 // Und zurück geben
+    return incomming;                 // Und zurÃ¼ck geben
 }
 bool waitI2C(void)
 {
@@ -370,7 +219,7 @@ bool waitI2C(void)
         if(Time + 200 < millisec)
             return 1;
     }
-    PIR1bits.SSPIF = 0;              // Flag zurücksetzten
-    // *Zum Beispiel: START-Sequenze, Übertragung von 8 Bit, ...
+    PIR1bits.SSPIF = 0;              // Flag zurÃ¼cksetzten
+    // *Zum Beispiel: START-Sequenze, Ãœbertragung von 8 Bit, ...
     return 0;
 }
