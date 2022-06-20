cf514b3efe79e9b0dac74cf6272593566d8c2f6b
BeFlE <83487780+BeFlE@users.noreply.github.com>
2022-06-20 15:07:30 +0200

    Update Demo.ino

--- Arduino/Demo.ino
+++ Arduino/Demo.ino
@@ -1,21 +1,27 @@
+#include <Wire.h>
+
+void setupI2C()      //call once 
+{
+    Wire.begin();           // join i2c bus as master
+}
 int getAveragedSensorValue(int Adr)
 {
-	int value;
-	int dump;
+  int value;
+  int dump;
 
-	Wire.beginTransmission(byte(Adr)); 	// transmit to device
-	Wire.write(byte(0x76));      		// command read value
-	Wire.endTransmission();      		// stop transmitting
+  Wire.beginTransmission(byte(Adr));  // transmit to device
+  Wire.write(byte(0x76));         // command read value
+  Wire.endTransmission();         // stop transmitting
 
-	delay(1);                   // maybe some delay is required
+  delay(1);                   // maybe some delay is required
 
-	Wire.requestFrom(byte(Adr), 2);    	// request 2 bytes from slave device 
-	if (2 <= Wire.available()) 		// if two bytes were received
-	{ 
-   		value = Wire.read();  	
-    		dump = Wire.read(); 
-	}
-	return value;
+  Wire.requestFrom(byte(Adr), 2);     // request 2 bytes from slave device 
+  if (2 <= Wire.available())    // if two bytes were received
+  { 
+      value = Wire.read();    
+        dump = Wire.read(); 
+  }
+  return value;
 }
 
 void setup() {
@@ -29,7 +35,7 @@ void loop() {
   // Wait a few seconds between measurements.
   delay(2000);
 
-  int hum = getAveragedSensorValue(int Adr);
+  int hum = getAveragedSensorValue(0x55);
   
   Serial.print("averaged humidity = ");
   Serial.println(hum);
