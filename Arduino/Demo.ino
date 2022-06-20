242932d69683d2716265ddfe097e6eb6529334c3
BeFlE <83487780+BeFlE@users.noreply.github.com>
2022-06-20 13:20:29 +0200

    Update Demo.ino

--- Arduino/Demo.ino
+++ Arduino/Demo.ino
@@ -1,4 +1,22 @@
-#include "SoMoSe_Arduino.h"
+int getAveragedSensorValue(int Adr)
+{
+	int value;
+	int dump;
+
+	Wire.beginTransmission(byte(Adr)); 	// transmit to device
+	Wire.write(byte(0x76));      		// command read value
+	Wire.endTransmission();      		// stop transmitting
+
+	delay(1);                   // maybe some delay is required
+
+	Wire.requestFrom(byte(Adr), 2);    	// request 2 bytes from slave device 
+	if (2 <= Wire.available()) 		// if two bytes were received
+	{ 
+   		value = Wire.read();  	
+    		dump = Wire.read(); 
+	}
+	return value;
+}
 
 void setup() {
   Serial.begin(9600);
