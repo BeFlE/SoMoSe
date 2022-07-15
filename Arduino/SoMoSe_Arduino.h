9b0701668d77e26b854f7fbf2e0452ab87e48c5d
cameo69 <44269764+cameo69@users.noreply.github.com>
2022-07-15 09:40:51 +0200

    Update SoMoSe_Arduino.h
    https://forum.arduino.cc/t/what-is-wprogram-h-library/125951/8

--- Arduino/SoMoSe_Arduino.h
+++ Arduino/SoMoSe_Arduino.h
@@ -1,4 +1,10 @@
 
+#if ARDUINO >= 100
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif
+
 void setupI2C();
 void setNewI2CAdr(int oldAdr, int newAdr);
 void setReferenceDry(int Adr,int newValue);
