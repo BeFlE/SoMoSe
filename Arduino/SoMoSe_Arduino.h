4f2f32a1ba2aca8399e79d30ee98cdc2eedd3058
BeFlE <83487780+BeFlE@users.noreply.github.com>
2024-04-30 12:39:59 +0200

    Update SoMoSe_Arduino.h

--- Arduino/SoMoSe_Arduino.h
+++ Arduino/SoMoSe_Arduino.h
@@ -14,3 +14,11 @@ int getSensorValue(int Adr);
 char getTemperatureValue(int Adr);
 int getReferenceDry(int Adr);
 int getReferenceWet(int Adr);
+
+//only for Somose versions > v3.4 (HW version)
+float SoMoSe_readHwVersion(uint8_t Adr);
+float SoMoSe_readFwVersion(uint8_t Adr);
+bool SoMoSe_readLowPowerMode(uint8_t Adr);
+void SoMoSe_setLowPowerMode(int Adr, bool turnOn);
+void SoMoSe_startMeassurment(int Adr, uint8_t Repetitions = 100);
+bool SoMoSe_MeassurementFinished(uint8_t Adr);
